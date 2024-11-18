31a5d12e1e96e0e83755783fb1ecba09e4f4efb0
Samet Sahin <sametsahinca@gmail.com>
2024-11-17 19:01:05 -0800

    static files integration has been done

--- h_project/core/views.py
+++ h_project/core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def test(request):
-    return HttpResponse("It is a test page")
+def index(request):
+    return render(request, 'core/index.html')
